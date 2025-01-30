aee841900c38d6b36140e61ad95a73251bf9a702
Elenadisa <elenadiazsantiago.92@gmail.com>
2025-01-30 14:33:20 +0100

    update figure abundance by taxa

--- functions.R
+++ functions.R
@@ -42,26 +42,24 @@ alpha_diversity_plot <- function(phy_object, group, alfa_metrics, my_comparisons
 #                           ABUNDANCE                                          #
 ################################################################################
 
-filter_abundance_table
 plot_abundance_by_taxa <- function(df, taxa, metric, group){
-  plt <- ggplot(df ,aes(x=reorder(get(taxa), get(metric)), y=get(metric), group=get(group), fill=get(group))) +
-    geom_bar(stat="identity",position="dodge")  +
+  plt <- ggplot(df ,aes(x=get(taxa), y=get(metric), group=get(group), fill=get(group))) +
+    geom_bar(colour="black", stat="identity",position="dodge")  +
     theme_classic() +
-    theme(axis.text.x = element_text(angle = 45, hjust = 1, face = "italic"), 
+    theme(axis.text.x = element_text(size = 18, angle = 90, hjust = 1, face = "italic"), 
           legend.title = element_blank(),
-          axis.title=element_text(size=14,face="bold"),
-          axis.text=element_text(size=12),
-          legend.text=element_text(size=12)) +
-    #scale_fill_grey(start=0, end=1) +
+          axis.title=element_text(size=20,face="bold"),
+          axis.text.y=element_text(size=18),
+          legend.text=element_text(size=20),
+          panel.border = element_rect(colour = "black", fill=NA, size=1)) +
+    scale_fill_grey(start = 0, end = 1) +
     geom_errorbar(aes(ymin=get(metric), ymax=get(metric)+sd),
-                  width=.2,                    # Width of the error bars
-                  position=position_dodge(.9)) +
-    ylab(metric) + xlab(taxa) 
+                  width=.5,                    # Width of the error bars
+                  position=position_dodge(.9)) 
   
   return(plt)
   
 }
-
 
 plot_abundance_heatmap<- function(df, metric, taxa_level, group){
   ggplot(df, aes(get(group), get(taxa_level), fill= get(metric))) + 
